4a5f7c94b91df051c990e97f4f636d454d95f20c
onwelo\hubert.ciupinski <hubert.ciupinski@onwelo.com>
2024-07-12 12:54:30 +0200

    Adjust project structure

--- tools/release.ps1
+++ tools/release.ps1
@@ -3,18 +3,20 @@ Set-Location $PSScriptRoot
 
 $name = 'PowershellHistory'
 $assembly = "Community.PowerToys.Run.Plugin.$name"
-$version = "v$((Get-Content ./plugin.json | ConvertFrom-Json).Version)"
+$projectPath = "../$name"
+$pluginPath = "$projectPath/plugin.json"
+$version = "v$((Get-Content $pluginPath | ConvertFrom-Json).Version)"
 $archs = @('x64', 'arm64')
-$tempDir = './out/temp'
+$tempDir = '../out/temp'
 
 git tag $version
 git push --tags
 
-Remove-Item ./out/*.zip -Recurse -Force -ErrorAction Ignore
+Remove-Item ../out/*.zip -Recurse -Force -ErrorAction Ignore
 foreach ($arch in $archs) {
-	$releasePath = "./bin/$arch/Release/net8.0-windows"
+	$releasePath = "$projectPath/bin/$arch/Release/net8.0-windows"
 
-	dotnet build -c Release /p:Platform=$arch
+	dotnet build $projectPath -c Release /p:Platform=$arch
 
 	Remove-Item "$tempDir/*" -Recurse -Force -ErrorAction Ignore
 	mkdir "$tempDir" -ErrorAction Ignore
@@ -25,8 +27,8 @@ foreach ($arch in $archs) {
 		"$releasePath/Images"
 	)
 	Copy-Item $items "$tempDir" -Recurse -Force
-	Compress-Archive "$tempDir" "./out/$name-$version-$arch.zip" -Force
+	Compress-Archive "$tempDir" "../out/$name-$version-$arch.zip" -Force
 }
 
-gh release create $version (Get-ChildItem ./out/*.zip)
+gh release create $version (Get-ChildItem ../out/*.zip)
 Pop-Location
